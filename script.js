5c6b566655183759d2d0854047a947f86cd0e1b7
Doug Enas <douglassenas@gmail.com>
2018-08-13 14:58:58 -0400

    through 17

--- script.js
+++ script.js
@@ -42,25 +42,63 @@ let result7 = document.createTextNode(JSON.stringify(lotrCitiesArray.slice(2, 5)
 number7.appendChild(result7)
 
 //number8
-{
-    const lotrCitiesArray = ["Mordor", "Gondor", "Rohan", "Beleriand", "Mirkwood", "Dead Marshes", "Rhun", "Harad"];
-    lotrCitiesArray.splice(2, 1)
-    let number8 = document.getElementById("number8")
-    let result8 = document.createTextNode(JSON.stringify(lotrCitiesArray))
-    number8.appendChild(result8)
-}
+
+
+lotrCitiesArray.splice(2, 1)
+let number8 = document.getElementById("number8")
+let result8 = document.createTextNode(JSON.stringify(lotrCitiesArray))
+number8.appendChild(result8)
+
 //number 9
-{
-    const lotrCitiesArray = ["Mordor", "Gondor", "Rohan", "Beleriand", "Mirkwood", "Dead Marshes", "Rhun", "Harad"];
-    lotrCitiesArray.splice(6)
-    let number9 = document.getElementById("number9")
-    let result9 = document.createTextNode(JSON.stringify(lotrCitiesArray))
-    number9.appendChild(result9)
-}
+
+
+lotrCitiesArray.splice(5)
+let number9 = document.getElementById("number9")
+let result9 = document.createTextNode(JSON.stringify(lotrCitiesArray))
+number9.appendChild(result9)
+
 
 //number 10
-{
+
+lotrCitiesArray.splice(2, 0, 'Rohan')
+let number10 = document.getElementById("number10")
+let result10 = document.createTextNode(JSON.stringify(lotrCitiesArray))
+number10.appendChild(result10)
 
 
+//number 11
 
-}
+lotrCitiesArray.splice(5, 1, 'Deadest Marshes')
+let number11 = document.getElementById("number11")
+let result11 = document.createTextNode(JSON.stringify(lotrCitiesArray))
+number11.appendChild(result11)
+
+//number 12
+
+let number12 = document.getElementById("number12")
+let result12 = document.createTextNode(JSON.stringify(bestThing.slice(0, 14)))
+number12.appendChild(result12)
+
+//number13
+
+let number13 = document.getElementById("number13")
+let result13 = document.createTextNode(JSON.stringify(bestThing.slice(-12)))
+number13.appendChild(result13)
+
+//number 14
+
+let number14 = document.getElementById("number14")
+let result14 = document.createTextNode(JSON.stringify(bestThing.slice(23, 38)))
+number14.appendChild(result14)
+
+//number 15
+
+let number15 = document.getElementById("number15")
+let result15 = document.createTextNode(JSON.stringify(bestThing.substring(0, 14)))
+number15.appendChild(result15)
+
+//number 16
+
+let number16 = document.getElementById("number16")
+let result16 = document.createTextNode(JSON.stringify(bestThing.substr(23, 15)))
+number16.appendChild(result16)
